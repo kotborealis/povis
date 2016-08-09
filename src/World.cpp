2ecdbb2dd2fd0da5884ebee0b846ee1379717164
kotborealis <kotborealis@gmail.com>
2016-08-09 13:15:15 +0300

    Switched to cmake

--- src/World.cpp
+++ src/World.cpp
@@ -1,7 +1,7 @@
 /*
  * World.cpp
  *
- *  Created on: 8 àâã. 2016 ã.
+ *  Created on: 8 ï¿½ï¿½ï¿½. 2016 ï¿½.
  *      Author: kotborealis
  */
 
@@ -11,25 +11,6 @@ namespace PovisEngine {
 
 World::World(Area* _map) {
 	map = _map;
-	background_texture = Game::i().g()->reserveTexture();
-	terrain_texture = Game::i().g()->reserveTexture();
-	foreground_texture = Game::i().g()->reserveTexture();
-
-	auto g = Game::i().g();
-	for(auto it = map->terrain_tiles.begin(); it != map->terrain_tiles.end(); it++){
-		Zone r = *it;
-		std::string file;
-		if(r.type == 0){
-			file="assets/textures/ground2.png";
-		}
-		else if(r.type == 1){
-			file="assets/textures/ground1.png";
-		}
-		else{
-			file="assets/textures/sky1.png";
-		}
-		textures.insert(std::pair<unsigned char, GLuint>(r.type,g->loadTexture(file)));
-	}
 }
 
 World::~World() {
