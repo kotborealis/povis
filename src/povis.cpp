f09525a4b0ed037ffa157145f484d785935cb7a2
kotborealis <kotborealis@gmail.com>
2016-10-08 20:18:31 +0300

    Added ability to specify screen size through program args

--- src/povis.cpp
+++ src/povis.cpp
@@ -5,8 +5,16 @@
 using namespace PovisEngine;
 
 int main(int argc, char* args[]){
+    unsigned int width = 800;
+    unsigned int height = 600;
+
+    if(argc >= 2){
+        width = (unsigned int)atoi(args[1]);
+        height = (unsigned int)atoi(args[2]);
+    }
+
     Logger::info("Starting");
-    Game::initialize("Povis", 800, 600);
+    Game::initialize("Povis", width, height);
     GameState* _ = new GameStateDemo();
     Game::i().pushState(_);
     Game::i().run();
