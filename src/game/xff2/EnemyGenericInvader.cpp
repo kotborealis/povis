73f946325a868959e2f869d918c7c134d8813028
kotborealis <kotborealis@gmail.com>
2016-12-21 00:34:58 +0300

    Only front-facing sprites now used

--- src/game/xff2/EnemyGenericInvader.cpp
+++ src/game/xff2/EnemyGenericInvader.cpp
@@ -9,9 +9,31 @@ namespace pse{
 
 
 EnemyGenericInvader::EnemyGenericInvader(int sprite_n){
-    if((sprite_n >= 9 && sprite_n <= 11) || (sprite_n >= 9 + 12 && sprite_n <= 11 + 12) ||
-       (sprite_n >= 9 + 12 * 3 && sprite_n <= 11 + 12 * 3)){
-        sprite_n -= 5;
+    switch(sprite_n){
+        case 0:
+            sprite_n = 1;
+            break;
+        case 1:
+            sprite_n = 4;
+            break;
+        case 2:
+            sprite_n = 7;
+            break;
+        case 3:
+            sprite_n = 49;
+            break;
+        case 4:
+            sprite_n = 52;
+            break;
+        case 5:
+            sprite_n = 55;
+            break;
+        case 6:
+            sprite_n = 58;
+            break;
+        default:
+            sprite_n = 0;
+            break;
     }
     sprite = std::make_shared<Sprite>(ResourceTexture->load("assets/xff2/textures/Doll_2.png"), 12, 8, sprite_n, 0,
                                       50);
